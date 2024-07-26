27a28d0a7a2fc1a888ecdaa3ae2214d281908725
Guille008 <guille15243342@gmail.com>
2024-07-26 14:24:44 -0300

    ProyectoJavaScrip

---
+++ javaScrip/index.js
@@ -0,0 +1,21 @@
+// Primero deberiamos cargar las variables pero
+// pero vamos a ver si es conveniente ponerlo como producto
+var total = 0;
+const envio = 2000;
+const producto1 = "menu01";
+var seleccion = 0
+var nombre = "Nombre"
+var apellido = "Apellido"
+var direccion = "Direccion"
+console.log("Bienvenido a RG burgers");
+nombre = prompt("Ingurese su nombre");
+apellido = prompt("Ingrese su apellido");
+console.log("Nos alegra tenerte por aqui", nombre, "estas son nuestas opciones de menu");
+alert(producto1);
+seleccion = prompt(nombre,"ingresa el menu que sea de tu preferencia");
+console.log("elegiste",seleccion);
+
+
+
+
+
