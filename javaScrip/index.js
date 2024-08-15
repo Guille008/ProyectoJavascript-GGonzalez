63a6b9f5d8ce121e7b0ec5910a148071fdeb9abe
Guille008 <guille15243342@gmail.com>
2024-08-14 21:41:29 -0300

    Actualizacion para preentrega 2

--- javaScrip/index.js
+++ javaScrip/index.js
@@ -1,13 +1,20 @@
-// Primero deberiamos cargar las variables pero
-// pero vamos a ver si es conveniente ponerlo como producto
 let total = 0;
 const envio = 2000;
-const menuPequeño = "1:Combo pequeño";
-const menuMediano = "2:Combo mediano";
-const menuGrande = "3:Combo grande";
-const precioPequeño = 5000;
-const precioMediano = 6500;
-const precioGrande = 8000;
+// const menuPequeño = "1:Combo pequeño";
+// const menuMediano = "2:Combo mediano";
+// const menuGrande = "3:Combo grande";
+// const precioPequeño = 5000;
+// const precioMediano = 6500;
+// const precioGrande = 8000;
+
+function Combos (nombre,precio){
+    this.nombre = nombre;
+    this.precio = precio;
+}
+
+const comboPequeño = new Combos("1: Combo Pequeño",5000);
+const comboMediano = new Combos("2:Combo Mediano",6500);
+const ComboGrande = new Combos("3: Combo Grande",8000);
 
 let seleccion = 0;
 let nombre = "Nombre";
@@ -15,19 +22,19 @@ let apellido = "Apellido";
 let direccion = "0";
 let delivery = "no";
 
-alert("Bienvenido a RG burgers");
-nombre = prompt("Ingurese su nombre");
-apellido = prompt("Ingrese su apellido");
-alert("Nos alegra tenerte por aqui"+" "+nombre);
-alert("Estas son nuestas opciones de menu que tenemos para vos"+"\n"+menuPequeño+"\n"+menuMediano+"\n"+menuGrande);
+// alert("Bienvenido a RG burgers");
+// nombre = prompt("Ingurese su nombre");
+// apellido = prompt("Ingrese su apellido");
+// alert("Nos alegra tenerte por aqui"+" "+nombre);
+// alert("Estas son nuestas opciones de menu que tenemos para vos"+"\n"+menuPequeño+"\n"+menuMediano+"\n"+menuGrande);
 seleccion = parseInt(prompt("Ingresa el numero de la opcion elegida"));
 
 if (seleccion == 1){
-    total = precioPequeño;
+    total =comboPequeño.precio;
 } else if (seleccion == 2){
-    total = precioMediano;
+    total = comboMediano.precio;
 } else if (seleccion == 3){
-    total = precioGrande;
+    total = ComboGrande.precio;
 } else {
     console.log("El dato ingresado es incorrecto");
 }
@@ -45,11 +52,11 @@ if (delivery == "Si"|| delivery == "si"){
 console.log(nombre,"Recibimos correctamente su pediodo\nDatos del cliente:\n");
 console.log("Nombre: ",nombre," ",apellido);
 if (seleccion == 1){
-    console.log("Menu", menuPequeño,"$"+precioPequeño);
+    console.log("Menu",comboPequeño);
 } else if (seleccion == 2){
-    console.log("Menu", menuMediano,"$"+precioMediano);
+    console.log("Menu", comboMediano);
 } else if (seleccion == 3){
-    console.log("Menu", menuGrande,"$"+precioGrande);
+    console.log("Menu", ComboGrande);
 }
 if (direccion != 0){
 console.log("Direccion de entrega: ",direccion);
@@ -60,25 +67,25 @@ let agregarPedido = prompt("Desea Agregar otro depido?")
 while(agregarPedido == "si" || agregarPedido=="si"){
     console.log("si queso un pedido");
     let clienteDos = prompt("Ingrese nombre para el otro pedio.");
-    alert("Estas son nuestas opciones de menu que tenemos para vos"+"\n"+menuPequeño+"\n"+menuMediano+"\n"+menuGrande);
+    alert("Estas son nuestas opciones de menu que tenemos para vos"+"\n"+comboPequeñoo+"\n"+comboMediano+"\n"+ComboGrande);
     let seleccionDos = parseInt(prompt("Ingresa el numero de la opcion elegida"));
     if (seleccionDos == 1){
-        total = total + precioPequeño;
+        total = total + comboPequeño.precio;
     } else if (seleccionDos == 2){
-        total = total + precioMediano;
+        total = total + comboMediano.precio;
     } else if (seleccionDos == 3){
-        total = total + precioGrande;
+        total = total + ComboGrande.precio;
     } else {
         console.log("El dato ingresado es incorrecto");
     }
     console.log(clienteDos,"Agragamos tu pedido correctamente");
     console.log("Detalle de pedido:")
     if (seleccionDos == 1){
-        console.log("Menu", menuPequeño,"$"+precioPequeño);
+        console.log("Menu", comboPequeño);
     } else if (seleccionDos == 2){
-        console.log("Menu", menuMediano,"$"+precioMediano);
+        console.log("Menu", comboMediano);
     } else if (seleccionDos == 3){
-        console.log("Menu", menuGrande,"$"+precioGrande);
+        console.log("Menu", ComboGrande);
     } else {
         console.log("Valor Incorrecto");
     }
